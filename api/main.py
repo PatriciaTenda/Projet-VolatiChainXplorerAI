2cbbc293a747fa57913fb189ce6fb97ef56911d0
PatriciaTenda <patricia.tdjouala@gmail.com>
2025-06-16 17:04:41 +0200

    Feat : Configuration des migrations avec alembic, créations des tables pour la bases de données postgresql et mise en place des schémas pydantics

---
+++ api/main.py
@@ -0,0 +1,10 @@
+"""
+    API principale pour VolatiChainXplorerAI.
+    Cette API expose des données relatives à la collecte des données qui sont les cours historiques du bitcoin sur la période de 2010 à 2025, des indicateurs macroéconomiques, ainsi que des informations financières complémentaires.
+    Elle fournit des points d'accès pour :
+    - Accéder à l'historique des cours du Bitcoin
+    - Obtenir les différents taux de la Banque Centrale Européenne
+    - Authentifier et gérer l'accès à l'API via un endpoint dédié
+    L'objectif est de faciliter l'exploration, l'analyse et l'intégration de ces données pour les développeurs et utilisateurs finaux.
+"""
+# Charger les librairies nécessaires
